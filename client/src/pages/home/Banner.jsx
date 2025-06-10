469fb9a0352bdd355ed114edf4ad4b5de7492053
Dre Cole <dcoleman1100@yahoo.com>
2025-06-10 18:38:04 -0400

    Update typo

--- client/src/pages/home/Banner.jsx
+++ client/src/pages/home/Banner.jsx
@@ -10,7 +10,7 @@ const Banner = () => {
         <h4>Stack: MongoDB, Express.js, React, Node.js</h4>
         <h1>DeAndre Coleman Ecommerce Portfolio Project</h1>
         {/* <p>Stack: MongoDB, Express.js, React, Node.js</p> */}
-        <p>Hey, thanks for visiting! This project is meant to mimic the functionality of an everyday ecommerce shop! Some features may be missing on initial visit. The github repo is in the links and features will be updated often.</p>
+        <p>Hey, thanks for visiting! This project is meant to mimic the functionality of an everyday ecommerce shop! Some features may be missing on initial visit. The GitHub repo is in the links and features will be updated often.</p>
         <button className='btn'><Link to='/shop'>Browse Project</Link></button>
       </div>
       <div className='header__image'>
