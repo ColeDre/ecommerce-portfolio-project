99d65a1646d42c6be282cb3a4acc7888e624de9b
Dre Cole <dcoleman1100@yahoo.com>
2025-06-10 23:01:55 -0400

    Fix nested a Error on router for Banner Component

--- client/src/pages/home/Banner.jsx
+++ client/src/pages/home/Banner.jsx
@@ -8,7 +8,7 @@ const Banner = () => {
       <div className='header__content'>
         <h4>Stack: MongoDB, Express.js, React, Node.js</h4>
         <h1>DeAndre Coleman Ecommerce Portfolio Project</h1>
-        <p>Hey, thanks for visiting my portfolio website! I'm a Full-Stack developer with over 5 years of professional experience. This project is meant to mimic the functionality of an everyday ecommerce web application. This project is on-going so some features may be missing on your initial visit. The <span className='link'><a href='#github'>GitHub repo</a></span> is linked below and on the <Link to='/contact' className='link'><a>Resume page</a></Link>, which also includes my more detailed experience and contact information. Thanks for visting!</p>
+        <p>Hey, thanks for visiting my portfolio website! I'm a Full-Stack developer with over 5 years of professional experience. This project is meant to mimic the functionality of an everyday ecommerce web application. This project is on-going so some features may be missing on your initial visit. The <span className='link'><a href='#github'>GitHub repo</a></span> is linked below and on the <span className='link'><a href='/contact'>Resume page</a></span>, which also includes my more detailed experience and contact information. Thanks for visting!</p>
         <button className='btn'><Link to='/shop'>Browse Project</Link></button>
       </div>
       <div className='header__image'>
