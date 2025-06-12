ed2dd6e673f8362d0eeca466fce33e4e1cd58827
Dre Cole <dcoleman1100@yahoo.com>
2025-06-11 20:56:07 -0400

    Implement star system in rating

--- client/src/components/Rating.jsx
+++ client/src/components/Rating.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
 
-const Rating = () => {
+const Rating = ({ rating }) => {
+  const stars = [];
+  
+  // 5 stars
+  for(let i = 1; i <= 5; i++){
+    let starIcon = '';
+    rating > i ? starIcon = 'ri-star-fill' : starIcon = 'ri-star-line';
+
+    // half-star functionality 
+    if (rating < i && (rating + .6) >= i) {
+      starIcon = 'ri-star-half-fill'
+    }
+    
+    stars.push(
+      <span key={i} className={`${starIcon}`}/>
+    )
+  }
+  
   return (
-    <div>
-        Rating
-    </div>
+    <div className='product__rating'>{stars}</div>
   )
 }
 
