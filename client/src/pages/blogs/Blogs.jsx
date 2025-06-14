9c9e8134462cef2182786947cb192dda384d90e9
Dre Cole <dcoleman1100@yahoo.com>
2025-06-14 16:52:15 -0400

    Refactor Blogs Component to take out card creation

--- client/src/pages/blogs/Blogs.jsx
+++ client/src/pages/blogs/Blogs.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React from "react";
+
+import BlogCard from "./BlogCard";
+import blogData from "../../data/blogs.json";
 
 const Blogs = () => {
   return (
-    <div>
+    <section className='section__container blog__container'>
+      <h2 className='capitalize section__header'>Check out our blogs</h2>
       
-    </div>
-  )
-}
+      <p className='section__subheader'>
+        Lorem, ipsum dolor sit amet consectetur adipisicing.
+      </p>
 
-export default Blogs
+      <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8'>
+
+        {blogData.map((blog, index) => <BlogCard blog={blog} blogId={index} key={index} />)}
+
+      </div>
+    </section>
+  );
+};
+
+export default Blogs;
