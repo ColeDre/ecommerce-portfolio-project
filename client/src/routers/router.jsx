3fc1ee7ae76a78f596c3d16864c37099f2e100d1
Dre Cole <dcoleman1100@yahoo.com>
2025-06-25 23:21:38 -0400

    Add category route to router

--- client/src/routers/router.jsx
+++ client/src/routers/router.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import Home from "../pages/home/Home";
 import NotFound from "../pages/NotFound";
+import CategoryPage from "../pages/categories/CategoryPage";
 
 const router = createBrowserRouter([
   {
@@ -12,6 +13,14 @@ const router = createBrowserRouter([
       {
         path: '/',
         element: <Home />
+      },
+      {
+        path: '/categories/:categoryName/:gender',
+        element: <CategoryPage />
+      },
+      {
+        path: '/categories/:categoryName',
+        element: <CategoryPage />
       },
       {
         path: '*',
